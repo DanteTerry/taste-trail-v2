9ad242868ae7ce7d4afabefe127aa378c5fb1092
DanteTerry <sinasa88@gmail.com>
2024-03-20 17:08:10 +0100

    fixed mobile cart responsive

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -4,18 +4,18 @@ import CartItem from "./CartItem";
 
 function Cart() {
   return (
-    <div className="h-full p-4">
+    <div className="h-full p-0 md:p-4 ">
       <h3 className="border-b-2 pb-2 text-xl font-semibold">
         Add Order to cart
       </h3>
 
-      <div className="flex  h-full flex-col justify-between">
-        <div className="mt-5 flex flex-col gap-5 rounded-lg border-b-2 bg-neutral-100 p-4 ">
+      <div className="flex  h-full flex-col justify-around">
+        <div className="mt-5 flex flex-col gap-5 rounded-lg border-b-2 bg-neutral-100 p-3 md:p-4 ">
           <CartItem />
           <CartItem />
           <CartItem />
         </div>
-        <div className="mt-5 w-full p-4 pb-14">
+        <div className="w-full p-4 md:mt-5">
           <div className="flex flex-col gap-2">
             <div className="flex justify-between text-lg">
               <h3 className="text-lg font-semibold text-black">Sub Total</h3>
@@ -27,13 +27,13 @@ function Cart() {
               <p className="font-semibold text-primary "> $ 5</p>
             </div>
 
-            <div className="mt-3 flex justify-between text-lg">
+            <div className="flex justify-between text-lg md:mt-3">
               <h3 className="text-lg font-semibold text-black">Total</h3>
               <p className="font-semibold text-primary "> $ 50</p>
             </div>
           </div>
 
-          <button className="mt-5 w-full rounded-lg bg-primary p-3 text-white">
+          <button className="mt-3 w-full rounded-lg bg-primary p-3 text-white md:mt-5">
             Checkout
           </button>
         </div>
--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -13,33 +13,42 @@ function CartItem() {
     setQuantity((prev) => (prev === 1 ? prev : prev - 1));
   };
   return (
-    <div className="flex justify-between">
-      <div className="flex gap-3 ">
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-3 ">
         <Image
           src={"/icons/indian.jpg"}
           alt="Food"
-          height={100}
-          width={100}
-          className="rounded-lg"
+          width={0}
+          height={0}
+          sizes="100%"
+          className="h-[100px] w-[100px] rounded-lg md:h-[100px] md:w-[100px]"
         />
 
         <div className="w-max">
           <h4 className="text-lg font-semibold">Chicken Roll</h4>
           <p className="text-sm text-gray-500">Indian</p>
 
-          <h4 className=" text-lg font-semibold">Price</h4>
+          <h4 className="w-max text-lg font-semibold">Price</h4>
           <p className="w-max rounded-full  bg-green-500 px-3 text-sm   text-white">
             $ 20
           </p>
         </div>
       </div>
 
-      <div className="flex w-[140px]  items-center justify-between self-end">
-        <Button className="text-2xl" onClick={handleDecrement}>
+      <div className="flex w-[110px] items-center justify-between self-end">
+        <Button
+          size={"icon"}
+          className="rounded-full text-2xl"
+          onClick={handleDecrement}
+        >
           -
         </Button>
-        <span className="text-lg font-semibold">{quantity}</span>
-        <Button onClick={handleIncrement} className="text-2xl">
+        <span className="text-xl font-semibold">{quantity}</span>
+        <Button
+          size={"icon"}
+          onClick={handleIncrement}
+          className="rounded-full text-2xl"
+        >
           +
         </Button>
       </div>
